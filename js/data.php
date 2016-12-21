d7cd1849b924aed963a53047d1f65551ab6cef43
microrice <875749367@qq.com>
2016-12-21 10:00:45 +0800

    分页插件

--- js/data.php
+++ js/data.php
@@ -5,8 +5,8 @@ header("Content-type:text/html;charset=utf-8");
 
    // echo $arr["att"];
     //echo $_POST["Start"];
-    $contents = array(array("ip"=>"127.0.0.1","name"=>"张立","status"=>"分析中...","comment"=>"暂无信息","date"=>"2016.7.1"),array("ip"=>"127.0.0.1","name"=>"张立","status"=>"分析中...","comment"=>"暂无信息","date"=>"2016.7.1"),array("ip"=>"127.0.0.1","name"=>"李立国","status"=>"分析中...","comment"=>"暂无信息","date"=>"2016.7.1"));
-    $json_arr = array("att"=>"whiteapply","start"=>"0","allcount"=>"20","length"=>"7","data"=>$contents);
+    $contents = array(array("id"=>"1","username"=>"admin","password"=>"aaaa222","email"=>"888888888@qq.com","lastlogin"=>"2016-7-1 12:09"),array("id"=>"1","username"=>"admin","password"=>"aaaa222","email"=>"888888888@qq.com","lastlogin"=>"2016-7-1 12:09"),array("id"=>"1","username"=>"admin","password"=>"aaaa222","email"=>"888888888@qq.com","lastlogin"=>"2016-7-1 12:09"));
+    $json_arr = array("att"=>"user-management","start"=>"0","allcount"=>"20","length"=>"7","data"=>$contents);
     $json_obj = json_encode($json_arr);
     echo $json_obj;
 ?>
